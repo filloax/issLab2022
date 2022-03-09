d2d4b70651fee14a85837bbed431125826e7a878
Filloax <filloax@gmail.com>
2022-03-09 12:12:09 +0100

    Add interfaces for radarSystem2022

---
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/interfaces/ILed.java
@@ -0,0 +1,7 @@
+package it.unibo.radarSystem22.interfaces;
+
+public interface ILed {
+    void turnOn();
+    void turnOff();
+    boolean getState();
+}
---
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/interfaces/IRadarDisplay.java
@@ -0,0 +1,5 @@
+package it.unibo.radarSystem22.interfaces;
+
+public interface IRadarDisplay {
+    void Update(String d, String a);
+}
