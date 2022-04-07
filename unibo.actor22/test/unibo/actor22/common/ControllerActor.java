4e05cbde76ee4a54a21b0b52ca6223246fec70e1
Filloax <filloax@gmail.com>
2022-04-07 12:49:38 +0200

    actor controller w sonar

--- unibo.actor22/test/unibo/actor22/common/ControllerActor.java
+++ unibo.actor22/test/unibo/actor22/common/ControllerActor.java
@@ -11,12 +11,21 @@ import unibo.actor22comm.utils.CommUtils;
  */
 public class ControllerActor extends QakActor22{
 	protected int numIter = 1;
-	protected IApplMessage getStateRequest ;
-	protected boolean on = true;
+	protected IApplMessage getStateRequest;
+	protected IApplMessage getDistanceRequest;
+	protected IApplMessage activateSonar;
+	protected IApplMessage deactivateSonar;
+	protected final int dlimit;
+	protected String justAsked;
 
 	public ControllerActor(String name  ) {
 		super(name);
 		getStateRequest  = ApplData.buildRequest(name,"ask", ApplData.reqLedState, ApplData.ledName);
+		getDistanceRequest = ApplData.buildRequest(name, "ask", ApplData.reqGetDistance, ApplData.sonarName);
+		activateSonar = ApplData.buildDispatch(name, "cmd", ApplData.cmdSonarActivate, ApplData.sonarName);
+		deactivateSonar = ApplData.buildDispatch(name, "cmd", ApplData.cmdSonarDeactivate, ApplData.sonarName);
+		dlimit = 40;
+		justAsked = null;
  	}
 
 	@Override
@@ -52,18 +61,26 @@ public class ControllerActor extends QakActor22{
 		}
 		forward( ApplData.turnOffLed );
 	}
-    protected void doControllerWork() {
-		CommUtils.aboutThreads(getName()  + " |  Before doControllerWork on=" + on );
+
+	protected void doControllerWork() { doControllerWork(-1); }
+
+    protected void doControllerWork(int distanceVal) {
+		CommUtils.aboutThreads(getName()  + " |  Before doControllerWork dist=" + distanceVal);
 		//wrongBehavior();
   		//ColorsOut.outappl( getName()  + " | numIter=" + numIter  , ColorsOut.GREEN);		
-	    if(numIter++ < 5) {
-	        if( numIter%2 == 1)
-				forward( ApplData.turnOnLed); //accesione
-	        else
-				forward( ApplData.turnOffLed); //spegnimento
-	        request(getStateRequest);
+	    if(numIter++ < 100) {
+			if (distanceVal >= 0)
+				if(distanceVal < dlimit)
+					forward(ApplData.turnOnLed); //accesione
+				else
+					forward(ApplData.turnOffLed); //spegnimento
+			else
+				forward(activateSonar);
+			justAsked = getDistanceRequest.msgContent();
+	        request(getDistanceRequest);
 	  	} else {
-	    	  forward( ApplData.turnOffLed);
+	    	  forward(ApplData.turnOffLed);
+			  forward(deactivateSonar);
 			  //ColorsOut.outappl(getName() + " | emit " + ApplData.endWorkEvent, ColorsOut.MAGENTA);
 	    	  //emit( ApplData.endWorkEvent );
 	  	}
@@ -71,8 +88,10 @@ public class ControllerActor extends QakActor22{
 	
 	protected void elabAnswer(IApplMessage msg) {
 		ColorsOut.outappl( getName()  + " | elabAnswer numIter=" + numIter + " "+ msg, ColorsOut.MAGENTA);
- 		CommUtils.delay(500);
-		doControllerWork();
+		CommUtils.delay(500);
+
+		if (msg.msgSender().equals(ApplData.sonarName) && justAsked.equals(ApplData.reqGetDistance))
+			doControllerWork(Integer.parseInt(msg.msgContent()));
 	}
 
 }
